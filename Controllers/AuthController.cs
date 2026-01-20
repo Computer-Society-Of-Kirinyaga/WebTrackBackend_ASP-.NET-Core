758c428b28079b17fd25b1d26f3411be1b4eff79
sarah kiarie <sarahwanjiruki1@gmail.com>
2026-01-20 15:24:07 +0300

    implemented login and register

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,23 +1,81 @@
+using System.Security.Claims;
 using Ecommerce.Api.Dtos;
 using Ecommerce.Api.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+
 
 namespace Ecommerce.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController(AppDbContext context) : ControllerBase
+    public class AuthController(AppDbContext context, IConfiguration _configuration ) : ControllerBase
     {
         private readonly AppDbContext _context = context; 
+        private readonly IConfiguration configuration = _configuration;
        
 
        [HttpPost("register")]
-       public ActionResult<Customer> Register(AuthDto request)
+       public async Task<ActionResult<Customer>> Register(AuthDto request)
         {
-             var customer = new Customer();
-            var HashedPassword = new PasswordHasher<Customer>().HashPassword(customer, request.PasswordHash);
+           if(_context.Customers.Any(u => u.Email == request.Email)) return BadRequest("email already exists");
+       var customer = new Customer
+       {
+           Email = request.Email
+       };
+       customer.PasswordHash = new PasswordHasher<Customer>().HashPassword(customer, request.PasswordHash);
+        _context.Customers.Add(customer);
+        await _context.SaveChangesAsync();
+
+        customer.PasswordHash = "";
+        return Ok(customer);
+        
         } 
+
+        [HttpPost("login")]
+        public async Task<ActionResult<Customer>> Login(AuthDto request)
+        {
+            var FoundCustomer = await _context.Customers.FirstOrDefaultAsync(c => c.Email == request.Email);
+            if(FoundCustomer == null) return Unauthorized("invalid log in credentials");
+
+            var result = new PasswordHasher<Customer>().VerifyHashedPassword(FoundCustomer, FoundCustomer.PasswordHash, request.PasswordHash);
+            if (result == PasswordVerificationResult.Failed) return Unauthorized("invalid Password");
+        
+        //generate jwt token and return
+        var token =  GenerateJwt(FoundCustomer);
+        return Ok(token);
+
+        }
+        public string GenerateJwt (Customer customer)
+        {
+            var claims = new List<Claim>
+            {
+              new Claim(ClaimTypes.NameIdentifier, customer.Id.ToString()), 
+              new(ClaimTypes.Email, customer.Email) 
+            };
+            
+            var Mykey = configuration["Jwt: Key"];
+            if (string.IsNullOrEmpty(Mykey)) throw new Exception("Jwt key is missing");
+
+            var key = new SymmetricSecurityKey (Encoding.UTF8.GetBytes(Mykey));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: configuration["Jwt: Issuer"],
+                audience: configuration["Jwt: Audience"] ,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(10),
+                signingCredentials: credentials
+            );
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
+        }
+       
     }
 }
--- obj/Debug/net8.0/Ecommerce.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/Ecommerce.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcb33646a08f4a6312f877e5f8ea31887b8aedfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
